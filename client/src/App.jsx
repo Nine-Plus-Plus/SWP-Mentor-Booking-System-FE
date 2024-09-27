cd63d70b1609fc53d52d9fab0be856d62c138300
hoangtmse <hoangtmse183217@fpt.edu.vn>
2024-09-27 18:29:44 +0700

    modify userService

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,48 @@
+import path from "./utils/path";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { ClassList, Login, MentorList, StudentHome } from "./components/index";
+import PublicLayout from "./pages/public/PublicLayout";
+import PublicHome from "./pages/public/PublicHome";
+import PublicAboutUs from "./pages/public/PublicAboutUs";
+import { ToastContainer } from "react-toastify";
+import { PublicStudent, PublicMentor } from "./pages/users";
+import PrivateRoute from "../middlewares/privateRoute";
+import { useUserStore } from "./store/useUserStore";
+import Activity from "./components/student/Activity";
+import StudentProfile from "./components/student/StudentProfile";
+import PublicAdmin from "./pages/users/PublicAdmin";
 
-import path from './utils/path'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import {ClassList, Login, MentorList, StudentHome} from './components/index'
-import PublicLayout from './pages/public/PublicLayout'
-import PublicHome from './pages/public/PublicHome'
-import PublicAboutUs from './pages/public/PublicAboutUs'
-import { ToastContainer } from 'react-toastify';
-import {PublicStudent, PublicMentor} from './pages/users'
-import PrivateRoute from '../middlewares/privateRoute'
-import { useUserStore } from './store/useUserStore'
+import AdminHome from "./components/admin/AdminHome";
+import UserList from "./components/admin/UserList";
 
 function App() {
-
-  const { isLoggedIn, role } = useUserStore()
+  const { isLoggedIn, role } = useUserStore();
   return (
-    <div> 
+    <div>
       <ToastContainer
         position="top-right" // Vị trí hiển thị toast
         autoClose={1000} // Thời gian tự động đóng sau 1 giây
         limit={3} // Giới hạn số lượng toast hiển thị
       />
       <Routes>
-        <Route path="/" element={<Navigate to={!isLoggedIn ? path.PUBLIC : path.PUBLIC_STUDENT} replace />} />
+        <Route
+          path="/"
+          element={
+            <Navigate
+              to={!isLoggedIn ? path.PUBLIC : path.PUBLIC_STUDENT}
+              replace
+            />
+          }
+        />
 
         {/* Route cho trang public */}
         <Route path={path.PUBLIC} element={<PublicLayout />}>
-          <Route index element={<PublicHome />}/>
-          <Route path={path.ABOUT_US} element={<PublicAboutUs />}/>
-          <Route path={path.LOGIN} element={<Login />}/>
+          <Route index element={<PublicHome />} />
+          <Route path={path.ABOUT_US} element={<PublicAboutUs />} />
+          <Route path={path.LOGIN} element={<Login />} />
         </Route>
 
-        {/* Route cho trang mentor */}
+        {/* Route cho trang student */}
         <Route
           path={path.PUBLIC_STUDENT}
           element={
@@ -42,6 +54,8 @@ function App() {
           <Route index element={<StudentHome />} />
           <Route path={path.STUDENT_VIEW_MENTOR} element={<MentorList />} />
           <Route path={path.STUDENT_VIEW_CLASS} element={<ClassList />} />
+          <Route path={path.STUDENT_ACTIVITY} element={<Activity />} />
+          <Route path={path.STUDENT_PROFILE} element={<StudentProfile />} />
         </Route>
 
         {/* Route cho trang mentor */}
@@ -57,9 +71,16 @@ function App() {
           <Route path={path.STUDENT_VIEW_CLASS} element={<ClassList />} />
         </Route>
         <Route path="*" element={<Navigate to={path.PUBLIC} replace />} />
+
+        {/* Route cho trang mentor */}
+        <Route path={path.PUBLIC_ADMIN} element={<PublicAdmin />}>
+          <Route index element={<AdminHome />} />
+          <Route path={path.STUDENT_PROFILE} element={<StudentProfile />} />
+          <Route path={path.ADMIN_USER_LIST} element={<UserList />} />
+        </Route>
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/utils/path.jsx
+++ client/src/utils/path.jsx
@@ -1,20 +1,27 @@
 const path = {
-    // Public
-    HOME: "/*",
-    LOGIN: "login",
-    PUBLIC: "public",
-    ABOUT_US: "about-us",
+  // Public
+  HOME: "/*",
+  LOGIN: "login",
+  PUBLIC: "public",
+  ABOUT_US: "about-us",
 
-    // Student
-    PUBLIC_STUDENT: "student",
-    STUDENT_HOME: "home",
-    STUDENT_VIEW_MENTOR: "view-mentor",
-    STUDENT_VIEW_CLASS: "view-class",
+  // Student
+  PUBLIC_STUDENT: "student",
+  STUDENT_HOME: "home",
+  STUDENT_VIEW_MENTOR: "view-mentor",
+  STUDENT_VIEW_CLASS: "view-class",
+  STUDENT_ACTIVITY: "activity",
+  STUDENT_PROFILE: "profile-student",
 
-    // Mentor
-    PUBLIC_MENTOR: "mentor",
-    MENTOR_HOME: "home",
-    MENTOR_VIEW_CLASS: "view-class",
-}
+  // Mentor
+  PUBLIC_MENTOR: "mentor",
+  MENTOR_HOME: "home",
+  MENTOR_VIEW_CLASS: "view-class",
 
-export default path
+  // Admin
+  PUBLIC_ADMIN: "admin",
+  ADMIN_HOME: "home",
+  ADMIN_USER_LIST: "user-list",
+};
+
+export default path;
