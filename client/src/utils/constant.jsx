0f9164799a319a4118059c942b5e58efd06c6586
Duckvit <vietnd2004@gmail.com>
2024-10-01 18:23:42 +0700

    <Add profile>

--- client/src/utils/constant.jsx
+++ client/src/utils/constant.jsx
@@ -47,7 +47,6 @@ export const menuNavbarItemsStudent = [
   {
     key: 'mentor',
     icon: <SolutionOutlined />,
-    label: 'Mentor',
     label: (
       <NavLink to={path.STUDENT_VIEW_MENTOR} className="text-white">
         Mentor
